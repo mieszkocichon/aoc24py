37c0c2e10b947e06e8f4a1bc2a8ca2a0cf40f3bc
mieszkocichon <mieszkocichon@protonmail.com>
2024-12-23 21:01:27 +0100

    Ceres Search

--- ceres_search/ceres_search.py
+++ ceres_search/ceres_search.py
@@ -1,8 +1,7 @@
 from monads.maybe import Just, Nothing
 from utils.load_file import read_file
 
-def get_ceres_search(file_path, word):
-    uppered_worlds = word.upper()
+def get_ceres_search(file_path):
     first_line_height = 0
 
     content = Just(file_path) \
@@ -11,10 +10,8 @@ def get_ceres_search(file_path, word):
 
     first_line_height = len(content.get_value()[0])
 
-    content.bind(lambda elements: validate_list_length(elements, first_line_height)) \
-        .orElse([])
-
-    return None
+    return content.bind(lambda elements: validate_list_length(elements, first_line_height)) \
+        .bind(lambda elements: calculate(elements))
 
 def validate_list_length(elements, first_line_height):
     for element in elements:
@@ -22,3 +19,25 @@ def validate_list_length(elements, first_line_height):
             return Nothing
 
     return Just(elements)
+
+def calculate(lists):
+    return [
+        tuple for tuple in get_xmas_neighbors_touples(lists)
+        if all('M' in element and 'A' in element and 'S' in element for element in tuple)
+    ]
+
+def get_xmas_neighbors_touples(lists):
+    return [
+        (
+            (lists[list_number - 1][element_position - 1]
+             + element
+             + lists[list_number + 1][element_position + 1],
+
+            lists[list_number - 1][element_position + 1]
+            + element
+            + lists[list_number + 1][element_position - 1])
+        )
+        for list_number, sub_list in enumerate(lists[1:-1], 1)
+        for element_position, element in enumerate(sub_list[1:-1], 1)
+        if element == "A"
+    ]
